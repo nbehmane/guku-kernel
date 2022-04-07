60f64b95f0a3c0fa91fbfff086c161f142f5f4fa
Nima Behmanesh <nbehmane@calpoly.edu>
2022-04-06 23:41:31 -0700

    Added very minimal VGA support. Added helpful macros for VGA support. Will need to move these to their own file eventually.

--- src/csrc/kmain.c
+++ src/csrc/kmain.c
@@ -1,19 +1,43 @@
+/**** BEGIN VGA ****/
+/* VGA Macros */
 #define VGA_BASE 0xb8000
+#define FG(color) ((color) & 0x0F)
+#define BG(color) ((color) << 4)
+#define VGA_CHAR(fg, bg, ch) (((FG((fg)) | BG((bg))) << 8) | (ch))
+#define VGA_BRIGHT(color) ((color) + 8)
+
+/* VGA Colors */
+#define VGA_BLACK      0
+#define VGA_BLUE       1
+#define VGA_GREEN      2
+#define VGA_CYAN       3
+#define VGA_RED        4
+#define VGA_MAGENTA    5
+#define VGA_BROWN      6
+#define VGA_GRAY 7
+
+/* VGA Globals */
 static unsigned short *vgaBuff = (unsigned short *)VGA_BASE;
 static int width = 80;
 static int height = 20;
 static int cursor = 0;
-static unsigned char color = (0 << 4) | (0xF & 0x0F);
 
-
-void VGA_display_char(char c)
+/* VGA Functions */
+extern void VGA_display_char(char c)
 {
-   vgaBuff[cursor] = ( color << 8) | c;
+   vgaBuff[cursor++] = VGA_CHAR(VGA_BRIGHT(VGA_GRAY), VGA_BLACK, c);
 }
+/**** END VGA ****/
 
+/**** BEGIN KMAIN ****/
 int kmain()
 {
-   VGA_display_char('A');
+   VGA_display_char('H');
+   VGA_display_char('E');
+   VGA_display_char('L');
+   VGA_display_char('L');
+   VGA_display_char('O');
+   VGA_display_char('!');
    for (;;)
       __asm__("hlt");
    return 0;
