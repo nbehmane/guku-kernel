fba855f2c737887ec1c0abc8bc37efabb9638ff6
Nima Behmanesh <nbehmane@calpoly.edu>
2022-04-06 23:54:24 -0700

    Added VGA_display_str. This is a premature function, and will change quite a bit.

--- src/csrc/kmain.c
+++ src/csrc/kmain.c
@@ -1,12 +1,12 @@
 /**** BEGIN VGA ****/
-/* VGA Macros */
+/** VGA Macros **/
 #define VGA_BASE 0xb8000
 #define FG(color) ((color) & 0x0F)
 #define BG(color) ((color) << 4)
 #define VGA_CHAR(fg, bg, ch) (((FG((fg)) | BG((bg))) << 8) | (ch))
 #define VGA_BRIGHT(color) ((color) + 8)
 
-/* VGA Colors */
+/** VGA Colors **/
 #define VGA_BLACK      0
 #define VGA_BLUE       1
 #define VGA_GREEN      2
@@ -14,30 +14,38 @@
 #define VGA_RED        4
 #define VGA_MAGENTA    5
 #define VGA_BROWN      6
-#define VGA_GRAY 7
+#define VGA_GRAY       7
 
-/* VGA Globals */
+/** VGA Globals **/
 static unsigned short *vgaBuff = (unsigned short *)VGA_BASE;
 static int width = 80;
 static int height = 20;
-static int cursor = 0;
 
-/* VGA Functions */
-extern void VGA_display_char(char c)
+/** VGA Functions **/
+extern void VGA_display_char(int row, int col, char c)
 {
-   vgaBuff[cursor++] = VGA_CHAR(VGA_BRIGHT(VGA_GRAY), VGA_BLACK, c);
+   vgaBuff[row*width + col] = VGA_CHAR(VGA_BRIGHT(VGA_GRAY), VGA_BLACK, c);
+}
+
+extern void VGA_display_str(int row, int col, char *str, int len)
+{
+   int i = 0;
+   if (len == 0)
+      while (*(str + i) != '\0')
+      {
+         VGA_display_char(row, col + i, *(str + i));
+         i++;
+      }
+   else 
+      for (; i < len; i++)
+         VGA_display_char(row, col + i, *(str + i));
 }
 /**** END VGA ****/
 
 /**** BEGIN KMAIN ****/
 int kmain()
 {
-   VGA_display_char('H');
-   VGA_display_char('E');
-   VGA_display_char('L');
-   VGA_display_char('L');
-   VGA_display_char('O');
-   VGA_display_char('!');
+   VGA_display_str(0, 0, "Hello, world!", 0); 
    for (;;)
       __asm__("hlt");
    return 0;
